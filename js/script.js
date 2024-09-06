6774a3245c870d5910ccf548c9688cc32d36256c
BeAmaral1 <bernardoamaral08@gmail.com>
2024-09-06 16:59:27 -0300

    Portfolio Responsivo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+function sendMail(){
+
+    event.preventDefault();
+
+    let params = {
+        subject : document.getElementById("subject").value,
+        full_name : document.getElementById("full_name").value,
+        phone : document.getElementById("phone").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value
+    };
+
+    console.log(params);
+
+    emailjs .send("service_00w4ili", "template_4bxgbeq",params)
+    console.log(document.getElementById('message').value);
+}
+
 
 const menuHamburguer = document.querySelector('.menu-hamburguer');
 menuHamburguer.addEventListener('click', () => {
